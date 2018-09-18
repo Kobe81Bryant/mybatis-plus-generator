0865ead0bda0c88b1f8a5f358328a75fcc35be56
KobeBryant <971054162@qq.com>
2018-09-18 17:38:02 +0800

    测试忽略

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 用户表 前端控制器
+ * </p>
+ *
+ * @author Kobe
+ * @since 2018-09-18
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
+
---
+++ src/main/java/com/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.mapper;
+
+import com.entity.User;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author Kobe
+ * @since 2018-09-18
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
