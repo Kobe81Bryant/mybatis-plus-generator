82eed77c574425bcf10aa485dcc6a9008a452e97
Kobe <971054162@qq.com>
2018-09-18 21:59:15 +0800

    666

---
+++ src/main/java/com/entity/User.java
@@ -0,0 +1,35 @@
+package com.entity;
+
+import com.baomidou.mybatisplus.enums.IdType;
+import java.math.BigDecimal;
+import com.baomidou.mybatisplus.annotations.TableId;
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotations.Version;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Kobe
+ * @since 2018-09-18
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    private String name;
+    private BigDecimal amount;
+
+
+}
---
+++ src/main/java/com/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.service.impl;
+
+import com.entity.User;
+import com.mapper.UserMapper;
+import com.service.UserService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Kobe
+ * @since 2018-09-18
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+}
